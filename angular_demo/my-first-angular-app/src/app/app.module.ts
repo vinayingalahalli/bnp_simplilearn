502a120e6dec6f5d7e8e9501038a2034cb1ecc6c
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-07-11 12:19:12 +0530

    angular binding

--- angular_demo/my-first-angular-app/src/app/app.module.ts
+++ angular_demo/my-first-angular-app/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
-
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular_demo/my-first-angular-app/src/app/player.ts
@@ -0,0 +1,15 @@
+export class Player {
+    id:number;
+    name:string;
+    age:number;
+    teamName:string;
+    avatar:string;
+
+    // constructor(id:number,name:string,age:number,teamName:string,avatar:string){
+    //     this.id=id;
+    //     this.name=name;
+    //     this.age=age;
+    //     this.teamName=teamName;
+    //     this.avatar=avatar;
+    // }
+}
